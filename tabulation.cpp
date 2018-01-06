2ceb81cfbeb9d7d88c88221b958fe7f0976ca2ae
sadstatue92 <danielradoev@gmail.com>
2018-01-06 02:59:03 +0200

    tabulations

---
+++ tabulation.cpp
@@ -0,0 +1,6 @@
+#include "tabulation.h"
+
+tabulation::tabulation()
+{
+
+}
---
+++ tabulation.h
@@ -0,0 +1,11 @@
+#ifndef TABULATION_H
+#define TABULATION_H
+
+
+class tabulation
+{
+public:
+    tabulation();
+};
+
+#endif // TABULATION_H
